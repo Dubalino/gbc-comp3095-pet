f4f0b63d98ef5b92e1e88832b3f093964a55afd6
Kevin <kevinjsilva94@gmail.com>
2020-11-14 17:06:17 -0500

    Closes #1

---
+++ pet-clinic-data/src/main/java/model/Pet.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Pet {
+
+    private String petType;
+    private String owner;
+    private String birthdate;
+
+    public Pet(){
+
+    }
+
+    public Pet(String PetType, String Owner, String Birthdate){
+        this.petType = PetType;
+        this.owner = Owner;
+        this.birthdate = Birthdate;
+
+    }
+
+
+}
